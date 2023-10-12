3a1f83beeb4eb0aa9c083f119814b6c8abd37fa7
Ernar <easkaruly1991@gmail.com>
2023-10-12 18:03:30 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a * b);
+
+        int c = 20;
+        double d = 2.5;
+
+        System.out.println(c / d);
+
+        int длина = 15;
+        int ширина = 9;
+
+        System.out.println(длина * ширина);
+        System.out.println(длина + ширина + длина + ширина);
+
+        int v = 1;
+        System.out.println(v++);
+        System.out.println(++v);
+
+        int x = 10;
+        System.out.println(x+=5);
+
+        int x1 = 25;
+        int x2 = 43;
+        System.out.println(x1 == x2);
+
+        int a1 = 10;
+        int a2 = 5;
+
+        int sum = a1 + a2;
+        int sum1 = a1 - a2;
+        int sum2 = a1 * a2;
+        int sum3 = a1 / a2;
+        int sum4 = a1 % a2;
+        System.out.println(sum);
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+        System.out.println(sum4);
+
+//        int фаренгейт = 32;
+//        int цельсия = 0;
+//        фаренгейт = цельсия;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
