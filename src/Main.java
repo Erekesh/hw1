67afbb6f7b11dfa27fe5ecf8f909b3df11b762dc
Ernar <easkaruly1991@gmail.com>
2023-10-18 22:06:01 +0600

    Work 18.10

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
+        //Задание №3
+        // 1.
+        String flowersName = "Roses";
+        int flowersNumber = 101;
+        System.out.println(flowersName);
+        System.out.println(flowersNumber);
 
-        System.out.println(a * b);
+        int a = 15;
+        int b = 10;
+        int c1 = a + b;
+        System.out.println(c1);
+
+        int x, z, y;
+        x = 4;
+        z = 5;
+        y = 3;
+        System.out.println(x + z +y);
+
+        int f, g, h, i;
+        f = g = h = i = 20;
+        System.out.println(f + g + h + i);
+
+        // 2.
+
+
+
+
+
+        // Задание №4
+        // 1.
+
+        int b1 = 10;
+        int b2 = 5;
+
+        System.out.println(b1 * b2);
+
+        //2.
 
         int c = 20;
         double d = 2.5;
 
         System.out.println(c / d);
+
+        //3.
 
         int длина = 15;
         int ширина = 9;
@@ -16,16 +51,24 @@ public class Main {
         System.out.println(длина * ширина);
         System.out.println(длина + ширина + длина + ширина);
 
+        //4.
+
         int v = 1;
         System.out.println(v++);
         System.out.println(++v);
 
-        int x = 10;
-        System.out.println(x+=5);
+        //5.
 
-        int x1 = 25;
-        int x2 = 43;
-        System.out.println(x1 == x2);
+        int x1 = 10;
+        System.out.println(x1+=5);
+
+        //6.
+
+        int x2 = 25;
+        int x3 = 43;
+        System.out.println(x2 == x3);
+
+        //7.
 
         int a1 = 10;
         int a2 = 5;
@@ -40,22 +83,6 @@ public class Main {
         System.out.println(sum2);
         System.out.println(sum3);
         System.out.println(sum4);
-
-//        int фаренгейт = 32;
-//        int цельсия = 0;
-//        фаренгейт = цельсия;
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
